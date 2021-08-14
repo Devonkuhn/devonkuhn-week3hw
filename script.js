5f2e59af4eba5c7fe913bf421990673076bf3305
kuhndev <86081460+immortalized513@users.noreply.github.com>
2021-08-14 18:40:52 -0400

    updated javascript
    Removed unwanted variables, prompts, confirms and arrays and added new variables and a function to change lowercase letters into uppercase letters

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
+var passwordLength;
+var upperConf;
+var lowerConf;
+var specialConf;
+var numberConf;
+var userChoice;
+
+
 var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// changing lowercase letters into uppercase
+var blankUpper = [];
+var toUpper = function(x) {
+  return x.toUpperCase();
+};
+upperCase = lowerCase.map(toUpper);
+
+
 var specialCharacter =   ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+
 // Write password to the #password input
 function writePassword() {
-
-  passwordlength = window.prompt("How many characters would you like your password to be?")
-  upperLetters = window.confirm("Do you want to include uppercase letters?")
-  lowerLetters = window.confirm("Do you want include lowercase letters?")
-  numbers = window.confirm("Do you want numeric values?")
-  specialCharacter = window.confirm("Do you want to include special characters?")
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
