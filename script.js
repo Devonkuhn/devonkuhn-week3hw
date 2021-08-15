51fe3e330cf39fc0eb30e03c9c78e1cfaa60fc5a
kuhndev <86081460+immortalized513@users.noreply.github.com>
2021-08-14 20:17:42 -0400

    updated javascript
    changed lowerLetters and specailCharacter variables also added if statements for prompt and confirm choices

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var numberConf;
 var userChoice;
 
 
-var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // changing lowercase letters into uppercase
 var blankUpper = [];
 var toUpper = function(x) {
@@ -19,7 +19,7 @@ var toUpper = function(x) {
 upperCase = lowerCase.map(toUpper);
 
 
-var specialCharacter =   ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
+var special =   ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
@@ -59,10 +59,87 @@ else {
   console.log("special" + specialConf)
 
 };
+// no answer
+if (!lowerConf && !upperConf && !numberConf && specialConf) {
+  userChoice = alert("You must choose a criteria");
+}
+// 4 true options
+else if (lowerConf && upperConf && numberConf && specialConf) {
+  userChoice = lowerCase.concat(upperCase, numbers, specialCharacter);
+  console.log(userChoice);
+}
+// 3 true options
+else if (lowerConf && upperConf && numberConf) {
+  userChoice = lowerCase.concat(upperCase, numbers);
+  console.log(userChoice)
+}
 
+else if (lowerConf && upperConf && specialConf) {
+  userChoice = lowerCase.concat(upperCase, special);
+  console.log(userChoice);
+}
 
+else if (lowerConf && numberConf && specialConf) {
+  userChoice = lowerCase.concat(numbers, special);
+  console.log(userChoice);
+}
 
+else if (upperConf && numberConf && specialConf) {
+  userChoice = upperCase.concat(numbers, special);
+  console.log(userChoice);
+}
 
+//2 true options
+else if (lowerConf && upperConf) {
+  userChoice = lowerCase.concat(upperCase);
+  console.log(userChoice);
+}
+
+else if (lowerConf && numberConf) {
+  userChoice = lowerCase.concat(numbers);
+  console.log(userChoice);
+}
+
+else if (lowerConf && specialConf) {
+  userChoice = lowerCase.concat(special);
+  console.log(userChoice);
+}
+
+else if (upperConf && numberConf) {
+  userChoice = upperCase.concat(numbers);
+  console.log(userChoice);
+}
+
+else if (upperConf && specialConf) {
+  userChoice = upperCase.concat(special);
+  console.log(userChoice);
+}
+
+else if (numberConf && specialConf) {
+  userChoice = numbers.concat(special);
+  console.log(userChoice);
+}
+
+// 1 true option
+else if (lowerConf) {
+  userChoice = lowerCase;
+  console.log(userChoice);
+}
+
+else if (upperConf) {
+  userChoice = blankUpper.concat(upperCase);;
+  console.log(userChoice);
+}
+
+else if (numberConf) {
+  userChoice = numbers;
+  console.log(userChoice);
+}
+
+else if (specialConf) {
+  userChoice = special;
+  console.log(userChoice);
+};
 
 
 
